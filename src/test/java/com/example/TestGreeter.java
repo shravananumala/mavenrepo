c8acab8e467f6e1311d6f6061ab143ff3e247d20
root <root@ip-172-31-37-179.ap-south-1.compute.internal>
2022-03-06 17:01:52 +0000

    test is changed for failure

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -24,7 +24,7 @@ public class TestGreeter {
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(someone), containsString("fail"));
   }
 
   @Test
